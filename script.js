989622f62ae196fbc4857d26e35efc579882481b
rachuureddy <113244477+rachuureddy@users.noreply.github.com>
2022-11-30 01:01:51 +0530

    hi
    hi

---
+++ script.js
@@ -0,0 +1,11 @@
+const uri = await pdfDoc.saveAsBase({dataUri: true });
+saveAs(uri,"course certificate.pdf",{autobom:true});
+}:
+const submitbtn=documnemt .getElementById ("submit")
+const inputVal=document.querySelector("#name");
+submitbtn.addEventListener("click",())
+{
+    const val = inputVal.ariaValueMax;
+    generatorPDF(val);
+}):
+}
